eda6665ac838ffd21069a1e3ca6aaf52df04e171
Muhammad Abdullah <m.abdula.idrees@gmail.com>
2026-08-02 01:48:53 +0500

    Update Day1WorkoutScreen.js

--- Day1WorkoutScreen.js
+++ Day1WorkoutScreen.js
@@ -60,7 +60,12 @@ export default function Day1WorkoutScreen({ navigation }) {
       <ProgressBar current={slide.setPosition} total={slide.totalSets} />
 
       {slide.type === 'rest' ? (
-        <RestTimer seconds={slide.seconds} onComplete={advance} />
+        <RestTimer
+          seconds={slide.seconds}
+          onComplete={advance}
+          completedSetNumber={slide.setPosition}
+          nextSetNumber={slide.setPosition + 1}
+        />
       ) : (
         <>
           <View style={styles.card}>
